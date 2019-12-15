db165010d8bae7e9d15c98e5061a3c431ba0b59d
George Duffett <george@georges-mbp.home>
2019-12-15 15:40:32 +0000

    Bringing up to stable for release v1.0.0

--- plot_peak_wavelengths.py
+++ plot_peak_wavelengths.py
@@ -6,12 +6,12 @@ import scipy.optimize as opt
 import scipy.signal as sig
 import os
 
-
 root = os.getcwd()
-root = '/run/user/1000/gvfs/smb-share:server=storage.its.york.ac.uk,share=physics/krauss/George/Optical measurements/19-11-13_bad_bulk/'
-ind, t, pk_wl = np.genfromtxt(root+'peak_wls.txt', skip_header=1, unpack=True)
+root = '../absorb_spec/'
+t, pk_wl, fit_wl = np.genfromtxt(root+'peak_wls.txt', skip_header=1, unpack=True)
 
 plt.plot(t, pk_wl, 'C0.', markersize=2)
+plt.plot(t, fit_wl, 'C1.', markersize=2)
 plt.grid(True)
 plt.xlabel('Time (minutes)')
 plt.ylabel('Peak wavelength (nm)')
--- timed_spectra.py
+++ timed_spectra.py
@@ -6,17 +6,16 @@ import numpy as np
 import scipy.optimize as opt
 import scipy.signal as sig
 from scipy.signal.windows import gaussian
-import mim
-import os
+import mim, sys, os
 
-root = os.getcwd()
-root = './spec/'
-fnames = [a for a in sorted(os.listdir(root)) if '.csv' in a]
-num_files = len(fnames)
+# root = os.getcwd()
+root = '../absorb_spec/'
+fpaths = [root+a for a in sorted(os.listdir(root)) if '.csv' in a]
+num_files = len(fpaths)
 
 ## Set and apply wavelength range
-wl1, wl2 = 500, 800
-wavs = np.genfromtxt(root+fnames[0], delimiter=';',
+wl1, wl2 = 550, 800
+wavs = np.genfromtxt(fpaths[0], delimiter=';',
     skip_header=33, skip_footer=1, unpack=True, usecols=(0))
 i1, i2 = np.argmin((wavs-wl1)**2), np.argmin((wavs-wl2)**2)
 
@@ -34,9 +33,9 @@ wavs = wavs[i1:i2]
 ## Starting guess
 popt_0 = [650, 60, 0.05, 0]
 
-for i, fname in enumerate(fnames):
+for i, fpath in enumerate(fpaths):
 
-    refl = np.genfromtxt(root+fname, delimiter=';',
+    refl = np.genfromtxt(fpath, delimiter=';',
         skip_header=33, skip_footer=1, unpack=True, usecols=1)
 
     ## Lowpass and truncate
@@ -44,19 +43,22 @@ for i, fname in enumerate(fnames):
     refl = refl[i1:i2]
 
     ## Find max wavelength
-    max_mim_wl.append(wavs[np.argmin(refl)])
+    max_wl = wavs[np.argmin(refl)]
+    max_mim_wl.append(max_wl)
+    popt_0[0] = max_wl
 
     # Fit a Lorentz curve
-    popt = opt.leastsq(mim.l_residuals, popt_0, args =(wavs, refl))[0]
+    popt = opt.leastsq(mim.l_residuals, popt_0, args=(wavs, refl))[0]
     fit_mim_wl.append(popt[0])
 
     ## Grab timestamp from inside file
-    with open(root+fname) as open_file:
+    with open(fpath) as open_file:
         time_stamp = [open_file.readline() for i in range(4)]
     time_stamp = (time_stamp[3].split(';')[1])[:-1]+'0'
 
     ## Convert timestamp to minutes and add to our time list, t.
-    hr, min, sec, ms = time_stamp[0:2], time_stamp[2:4] , time_stamp[4:6], time_stamp[6:]
+    hr, min = time_stamp[0:2], time_stamp[2:4]
+    sec, ms = time_stamp[4:6], time_stamp[6:]
     t_minutes = int(hr)*60+(int(min))+(int(sec)/60)
     t.append(t_minutes)
 
@@ -65,7 +67,7 @@ for i, fname in enumerate(fnames):
 # Start from t=0
 t = t-np.min(t)
 
-peak_wl_output = np.vstack((t, fit_mim_wl)).T
+peak_wl_output = np.vstack((t, max_mim_wl, fit_mim_wl)).T
 np.savetxt(root+'peak_wls.txt', peak_wl_output,
     delimiter='\t',
-    header='time(min)\t\t\tpeak wavelength (nm)')
+    header='time(min)\t\t\tMax wavelength (nm)\t\t\tpeak wavelength (nm)')
