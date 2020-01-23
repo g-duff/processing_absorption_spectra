e8cd52042b84f693206d82c3f1afd8716e9c16bf
George Duffett <george.duffett@york.ac.uk>
2020-01-23 10:55:09 +0000

    Fixed timestamp function

--- mim.py
+++ mim.py
@@ -23,12 +23,17 @@ def l_residuals(params, x, y_meas):
     return ressq
 
 def timestamp(file):
+    ''' Grabs timestamp from an open .csv CCS spectrum file'''
     time_stamp = [file.readline() for i in range(4)]
     time_stamp = (time_stamp[3].split(';')[1])[:-1]+'0'
 
     ## Convert timestamp to minutes and add to our time list, t.
-    hr, min = time_stamp[0:2], time_stamp[2:4]
-    sec, ms = time_stamp[4:6], time_stamp[6:]
+    if len(time_stamp) == 9:
+        hr, min = time_stamp[0:2], time_stamp[2:4]
+        sec, ms = time_stamp[4:6], time_stamp[6:]
+    elif len(time_stamp) == 8:
+        hr, min = time_stamp[0:1], time_stamp[1:3]
+        sec, ms = time_stamp[3:5], time_stamp[5:]
     t_minutes = int(hr)*60+int(min)+int(sec)/60
     return t_minutes
 
