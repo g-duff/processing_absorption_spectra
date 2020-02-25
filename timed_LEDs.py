c89be9caa56b5252e479c1e7e5c04ce7b0432d7b
George Duffett <george.duffett@me.com>
2020-02-25 00:05:06 +0000

    Better fig labels

--- timed_LEDs.py
+++ timed_LEDs.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# import concurrent.futures
 from numpy import loadtxt, mean, std, array, flatnonzero
 import matplotlib
 import matplotlib.pyplot as plt
@@ -10,7 +9,7 @@ font = {'size': 14}
 matplotlib.rc('font', **font)
 
 # Load data
-fpath = Path('/home/gd681/Desktop/bulk_trial/Di_to_15pc_NaCl.txt')
+fpath = Path('/Users/george/Desktop/Di_to_15pc_NaCl.txt')
 time, LED1, LED2, *voltages = loadtxt(fpath, skiprows=1,unpack=True)
 voltages = array(voltages)
 
@@ -27,11 +26,23 @@ V_mean, V_std = mean(voltages, 0), std(voltages, 0)
 V1 = V_mean[flatnonzero(LED1)]
 V2 = V_mean[flatnonzero(LED2)]
 
-plt.plot(time, V1)
-plt.plot(time, V2)
-plt.show()
+# Individual voltages
+fig_indiv, (ax1, ax2) = plt.subplots(nrows=2, sharex=True)
+ax1.plot(time, V1, 'C0')
+ax2.plot(time, V2, 'C1')
+ax1.set_ylabel('Voltage (V)')
+ax2.set_ylabel('Voltage (V)')
+ax2.set_xlabel('Time (minutes)')
+ax1.grid(True)
+ax2.grid(True)
+fig_indiv.tight_layout()
 
-# Plot
-plt.plot(time, V2/V1)
-plt.tight_layout()
+# Voltage ratio
+fig_ratio, ax_ratio = plt.subplots()
+ax_ratio.plot(time, V2/V1, 'C0')
+ax_ratio.set_xlabel('Time (minutes)')
+ax_ratio.set_ylabel('Voltage ratio')
+ax_ratio.grid(True)
+fig_ratio.tight_layout()
+
 plt.show()
